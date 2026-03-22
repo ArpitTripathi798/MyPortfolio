eae9288ed3f5886c1bc07ba211d4604f0ef04d9e
Arpit Tripathi <arpit786tripathi@gmail.com>
2026-03-22 07:55:23 +0530

    Update ProjectSection.jsx

--- src/pages/ProjectSection.jsx
+++ src/pages/ProjectSection.jsx
@@ -9,7 +9,7 @@ function ProjectSection() {
 
       <div className="mt-20 md:mt-0 space-y-10 md:space-y-0 md:p-20 flex justify-around flex-wrap">
 
-        {/* 🔥 AI Resume Analyzer */}
+     
         <ProjectCard
           imageLink="/Images/Ai-resume.jpg"
           title="AI Resume Analyzer"
@@ -18,7 +18,7 @@ function ProjectSection() {
           liveDemo="https://ai-resume-analyzer-nu-lemon.vercel.app/"
         />
 
-        {/* 🔗 URL Shortener */}
+     
         <ProjectCard
           imageLink="/Images/Url.jpg"
           title="URL Shortener"
@@ -27,13 +27,22 @@ function ProjectSection() {
           liveDemo="https://url-shortner-khaki-ten.vercel.app/"
         />
 
-        {/* 📌 Job Application Tracker */}
+      
         <ProjectCard
           imageLink="/Images/Job.jpg"
           title="Job Application Tracker"
           description="Track job applications with dashboard and authentication."
           github="https://github.com/ArpitTripathi798/Job-Tracker"
           liveDemo="https://job-tracker-two-ivory.vercel.app/"
+        />
+
+      
+        <ProjectCard
+          imageLink="/Images/wifi.jpg"
+          title="WiFi QR Code Generator"
+          description="Generate and scan WiFi QR codes using HTML, CSS and JavaScript."
+          github="https://github.com/ArpitTripathi798/Wifi-QR-Code"
+          liveDemo="https://wifi-qr-code-five.vercel.app"
         />
 
       </div>
